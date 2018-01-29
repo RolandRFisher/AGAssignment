333b0a9b209b245b78142425bdc52a127e0a013f
RolandRFisher <Pluto@PLUTO>
2018-01-29 23:07:34 +0200

    Add tweet length limit to report generator

--- Core/Interfaces/IReport.cs
+++ Core/Interfaces/IReport.cs
@@ -6,6 +6,6 @@ namespace Core.Interfaces
     public interface IReport
     {
         IEnumerable<TwitterModel> GetReport();
-        IEnumerable<TwitterModel> GenerateReport(IEnumerable<Users> userList, IEnumerable<Tweet> tweets);
+        IEnumerable<TwitterModel> GenerateReport(IEnumerable<Users> userList, IEnumerable<Tweet> tweets, int tweetLengthLimit);
     }
 }
--- TwitterService/Twitter/Report.cs
+++ TwitterService/Twitter/Report.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using Core.Interfaces;
 using Core.Models;
@@ -8,10 +10,12 @@ namespace Service.Twitter
     public class Report:IReport
     {
         private readonly ITwitterRepository _twitterRepository;
+        private int _tweetLengthLimit;
 
         public Report(ITwitterRepository twitterRepository)
         {
             _twitterRepository = twitterRepository;
+            _tweetLengthLimit = Convert.ToInt32(ConfigurationManager.AppSettings["LimitTweetTo"]);
         }
 
         #region public methods
@@ -22,10 +26,10 @@ namespace Service.Twitter
             var tweets = _twitterRepository.GetTweets();
 
 
-            return GenerateReport(userList.OrderBy(users => users.UserId).ToList(), tweets);
+            return GenerateReport(userList.OrderBy(users => users.UserId).ToList(), tweets, _tweetLengthLimit);
         }
 
-        public IEnumerable<TwitterModel> GenerateReport(IEnumerable<Users> userList, IEnumerable<Tweet> tweets)
+        public IEnumerable<TwitterModel> GenerateReport(IEnumerable<Users> userList, IEnumerable<Tweet> tweets, int tweetLengthLimit)
         {
             var twitterReport = new List<TwitterModel>();
             if (tweets == null) return twitterReport;
@@ -39,16 +43,18 @@ namespace Service.Twitter
                 var twts = new List<Tweet>();
                 foreach (var userTweet in tweets)
                 {
+                    var uTweet = userTweet.UserTweet.Substring(0, Math.Min(tweetLengthLimit, userTweet.UserTweet.Length));
+
                     if (userTweet.UserId == user.UserId)
                     {
-                        twts.Add(new Tweet() { UserId = userTweet.UserId, UserTweet = userTweet.UserTweet });
+                        twts.Add(new Tweet() { UserId = userTweet.UserId, UserTweet = uTweet });
                     }
 
                     foreach (var followedUser in followsList)
                     {
                         if (followedUser == userTweet.UserId)
                         {
-                            twts.Add(new Tweet() { UserId = userTweet.UserId, UserTweet = userTweet.UserTweet });
+                            twts.Add(new Tweet() { UserId = userTweet.UserId, UserTweet = uTweet });
                         }
                     }
                 }
